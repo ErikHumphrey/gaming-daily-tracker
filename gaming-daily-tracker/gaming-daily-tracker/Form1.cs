10fb8410c4a175bc6b1a4e39e1bae7e9d2dc4a87
Erik Humphrey <ContactErikH@gmail.com>
2016-06-11 21:16:14 -0400

    first experiment

---
+++ gaming-daily-tracker/gaming-daily-tracker/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gaming_daily_tracker
+{
+    public partial class frmGamingDailyTracker : Form
+    {
+        public frmGamingDailyTracker()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTimeLeftGW2.Text = lblTimeLeftBlackDesertOnline.Text = lblTimeLeftTheDivision.Text = lblTimeLeftTI6.Text = GetTimeUntilNextHour(20).ToString().Substring(0, 8);
+            lblTimeLeftGOG.Text = GetTimeUntilNextHour(9).ToString().Substring(0, 5);
+            lblTimeLeftBrawlhalla.Text = lblTimeLeftHearthstone.Text = lblTimeLeftWow.Text = lblTimeLeftHeroes.Text = GetTimeUntilNextHour(4).ToString().Substring(0, 8);
+            tmrUpdater.Start();
+        }
+
+        public static TimeSpan GetTimeUntilNextHour(int hour)
+        {
+            var currentTime = DateTime.Now;
+            var desiredTime = new DateTime(DateTime.Now.Year,
+                DateTime.Now.Month, DateTime.Now.Day, hour, 0, 0);
+            return currentTime.Hour >= hour ?
+                (desiredTime.AddDays(1) - currentTime) :
+                desiredTime - currentTime;
+        }
+
+        private void tmrUpdater_Tick(object sender, EventArgs e)
+        {
+            lblTimeLeftGW2.Text = lblTimeLeftBlackDesertOnline.Text = lblTimeLeftTheDivision.Text = GetTimeUntilNextHour(20).ToString().Substring(0, 8);
+            lblTimeLeftGOG.Text = GetTimeUntilNextHour(9).ToString().Substring(0, 8);
+            lblTimeLeftBrawlhalla.Text = lblTimeLeftHearthstone.Text = lblTimeLeftWow.Text = lblTimeLeftHeroes.Text = GetTimeUntilNextHour(4).ToString().Substring(0, 8);
+            lblTimeLeftHearthstone.Text = GetTimeUntilNextHour(3).ToString().Substring(0, 8);
+        }
+    }
+
+
+}
---
+++ gaming-daily-tracker/gaming-daily-tracker/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("gaming-daily-tracker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gaming-daily-tracker")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c1bb8bd4-9051-495f-bd07-10fa35183412")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
